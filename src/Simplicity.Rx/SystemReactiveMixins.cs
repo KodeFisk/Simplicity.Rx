7bf24a2869270c1d2571f61cb12a4dbc7bec0811
Unknown <jonstodle@outlook.com>
2018-01-07 23:02:41 +0100

    Add IObservable<T>.CatchAndReturn()

--- src/Simplicity.Rx/SystemReactiveMixins.cs
+++ src/Simplicity.Rx/SystemReactiveMixins.cs
@@ -14,5 +14,14 @@ namespace System.Reactive
         /// <param name="source">The source observable.</param>
         /// <typeparam name="T">The type of the source observable.</typeparam>
         public static IObservable<Unit> ToSignal<T>(this IObservable<T> source) => source.Select(_ => Unit.Default);
+
+        /// <summary>
+        /// Catches any error that might occur and return an observable with a single given value.
+        /// </summary>
+        /// <returns>An observable with a single given value.</returns>
+        /// <param name="source">The source observable to catch errors on.</param>
+        /// <param name="returnValue">The value to be sent in the returning observable.</param>
+        /// <typeparam name="T">The type of the signal inside the observable.</typeparam>
+        public static IObservable<T> CatchAndReturn<T>(this IObservable<T> source, T returnValue) => source.Catch(Observable.Return(returnValue));
     }
 }
