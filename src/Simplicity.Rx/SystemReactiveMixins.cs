79f3a7f10a944d070fa3a960b6b9ce4c5e947d87
Unknown <jonstodle@outlook.com>
2018-01-07 23:02:15 +0100

    Add IObservable<T>.ToSignal()

---
+++ src/Simplicity.Rx/SystemReactiveMixins.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Text;
+
+namespace System.Reactive
+{
+    public static class SystemReactiveMixins
+    {
+        /// <summary>
+        /// Converts an <c>IObservable&lt;T&gt;</c> to an <c>IObservable&lt;Unit&gt;</c>.
+        /// </summary>
+        /// <returns>An <c>IObservable&lt;Unit&gt;</c></returns>
+        /// <param name="source">The source observable.</param>
+        /// <typeparam name="T">The type of the source observable.</typeparam>
+        public static IObservable<Unit> ToSignal<T>(this IObservable<T> source) => source.Select(_ => Unit.Default);
+    }
+}
