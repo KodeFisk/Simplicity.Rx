590c9836fce7122d705b2464dcc22ef09461d9c8
Unknown <jonstodle@outlook.com>
2018-01-07 23:12:55 +0100

    Add IObservable<T>.GetEvents()

--- src/Simplicity.Rx/SystemReactiveMixins.cs
+++ src/Simplicity.Rx/SystemReactiveMixins.cs
@@ -39,5 +39,23 @@ namespace System.Reactive
         /// <param name="source">The observable to filter.</param>
         /// <typeparam name="T">The the of the signal inside the observable.</typeparam>
         public static IObservable<T> WhereNotNull<T>(this IObservable<T> source) => source.Where(value => value != null);
+
+        /// <summary>
+        /// Creates an observable that signals every time the event with the given name is fired
+        /// </summary>
+        /// <returns>An observable that signals every time the event is fired.</returns>
+        /// <param name="source">The object containing the event.</param>
+        /// <param name="eventName">Name of the event.</param>
+        /// <typeparam name="T">The type of the class contains the event.</typeparam>
+        public static IObservable<EventPattern<object>> GetEvents<T>(this T source, string eventName) => Observable.FromEventPattern(source, eventName);
+
+        /// <summary>
+        /// Creates an observable that signals every time the event with the given name is fired
+        /// </summary>
+        /// <returns>An observable that signals ever time the event is fired.</returns>
+        /// <param name="source">The object containing the event.</param>
+        /// <param name="eventName">Name of the event.</param>
+        /// <typeparam name="TEventArgs">The type of event arguments emitted by the event.</typeparam>
+        public static IObservable<EventPattern<TEventArgs>> GetEvents<TEventArgs>(this object source, string eventName) => Observable.FromEventPattern<TEventArgs>(source, eventName);
     }
 }
