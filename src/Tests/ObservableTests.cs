09ba1d36b0261dbac8fbb59bbbafde41f3b2ef45
Unknown <jonstodle@outlook.com>
2018-02-08 00:10:30 +0100

    Add ToSignal test

---
+++ src/Tests/ObservableTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using Xunit;
+
+namespace Tests
+{
+    public class ObservableTests
+    {
+        [Fact]
+        public void ToSignalShouldConvertNextSignalsToUnit()
+        {
+            var result = new List<object>();
+
+            Observable.Range(0, 5)
+                .ToSignal()
+                .Subscribe(value => result.Add(value));
+            
+            Assert.True(result.All(value => value.GetType() == typeof(Unit)));
+        }
+    }
+}
