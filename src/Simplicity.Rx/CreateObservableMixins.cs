d20afb5062edf45b3571127fdf1a7f6a541970e7
Unknown <jonstodle@outlook.com>
2018-02-08 01:12:02 +0100

    Use This as parameter name for extended instance

--- src/Simplicity.Rx/CreateObservableMixins.cs
+++ src/Simplicity.Rx/CreateObservableMixins.cs
@@ -10,34 +10,34 @@ namespace System.Reactive.Linq
         /// <summary>
         /// Creates an observable that starts the <c>Task</c> when subscribed to
         /// </summary>
-        /// <param name="source">The <c>Task</c> to convert.</param>
+        /// <param name="This">The <c>Task</c> to convert.</param>
         /// <returns></returns>
-        public static IObservable<Unit> ToColdObservable(this Task source) => Observable.FromAsync(() => source);
+        public static IObservable<Unit> ToColdObservable(this Task This) => Observable.FromAsync(() => This);
 
         /// <summary>
         /// Creates an observable that starts the <c>Task</c> when subscribed to
         /// </summary>
         /// <typeparam name="T">The type of the next signals.</typeparam>
-        /// <param name="source">The <c>Task</c> to convert.</param>
+        /// <param name="This">The <c>Task</c> to convert.</param>
         /// <returns></returns>
-        public static IObservable<T> ToColdObservable<T>(this Task<T> source) => Observable.FromAsync(() => source);
+        public static IObservable<T> ToColdObservable<T>(this Task<T> This) => Observable.FromAsync(() => This);
 
         /// <summary>
         /// Creates an observable that signals every time the event with the given name is fired
         /// </summary>
         /// <returns>An observable that signals every time the event is fired.</returns>
-        /// <param name="source">The object containing the event.</param>
+        /// <param name="This">The object containing the event.</param>
         /// <param name="eventName">Name of the event.</param>
         /// <typeparam name="T">The type of the class contains the event.</typeparam>
-        public static IObservable<EventPattern<object>> GetEvents<T>(this T source, string eventName) => Observable.FromEventPattern(source, eventName);
+        public static IObservable<EventPattern<object>> GetEvents<T>(this T This, string eventName) => Observable.FromEventPattern(This, eventName);
 
         /// <summary>
         /// Creates an observable that signals every time the event with the given name is fired
         /// </summary>
         /// <returns>An observable that signals ever time the event is fired.</returns>
-        /// <param name="source">The object containing the event.</param>
+        /// <param name="This">The object containing the event.</param>
         /// <param name="eventName">Name of the event.</param>
         /// <typeparam name="TEventArgs">The type of event arguments emitted by the event.</typeparam>
-        public static IObservable<EventPattern<TEventArgs>> GetEvents<TEventArgs>(this object source, string eventName) => Observable.FromEventPattern<TEventArgs>(source, eventName);
+        public static IObservable<EventPattern<TEventArgs>> GetEvents<TEventArgs>(this object This, string eventName) => Observable.FromEventPattern<TEventArgs>(This, eventName);
     }
 }
--- src/Simplicity.Rx/ObservableMixins.cs
+++ src/Simplicity.Rx/ObservableMixins.cs
@@ -11,82 +11,82 @@ namespace System.Reactive.Linq
         /// Converts an <c>IObservable&lt;T&gt;</c> to an <c>IObservable&lt;Unit&gt;</c>.
         /// </summary>
         /// <returns>An <c>IObservable&lt;Unit&gt;</c></returns>
-        /// <param name="source">The source observable.</param>
+        /// <param name="This">The source observable.</param>
         /// <typeparam name="T">The type of the source observable.</typeparam>
-        public static IObservable<Unit> ToSignal<T>(this IObservable<T> source) => source.Select(_ => Unit.Default);
+        public static IObservable<Unit> ToSignal<T>(this IObservable<T> This) => This.Select(_ => Unit.Default);
 
         /// <summary>
         /// Project every item into an observable and subscribes only to the most recent one. Like <c>SelectMany</c> but uses <c>Switch</c> instead of <c>Merge</c>.
         /// </summary>
         /// <typeparam name="T">The type of the source observable.</typeparam>
         /// <typeparam name="TResult">The type of the resulting observable.</typeparam>
-        /// <param name="source">The observable to project.</param>
+        /// <param name="This">The observable to project.</param>
         /// <param name="selector">The selector to use when projecting each item.</param>
         /// <returns>An <c>IObservable&lt;TResult&gt;</c></returns>
-        public static IObservable<TResult> SelectSwitch<T, TResult>(this IObservable<T> source, Func<T, IObservable<TResult>> selector) => source.Select(selector).Switch();
+        public static IObservable<TResult> SelectSwitch<T, TResult>(this IObservable<T> This, Func<T, IObservable<TResult>> selector) => This.Select(selector).Switch();
 
         /// <summary>
         /// Project every item and it's index into an observable and subscribes only to the most recent one. Like <c>SelectMany</c> but uses <c>Switch</c> instead of <c>Merge</c>.
         /// </summary>
         /// <typeparam name="T">The type of the source observable.</typeparam>
         /// <typeparam name="TResult">The type of the resulting observable.</typeparam>
-        /// <param name="source">The observable to project.</param>
+        /// <param name="This">The observable to project.</param>
         /// <param name="selector">The selector to use when projecting each item.</param>
         /// <returns>An <c>IObservable&lt;TResult&gt;</c></returns>
-        public static IObservable<TResult> SelectSwitch<T, TResult>(this IObservable<T> source, Func<T, int, IObservable<TResult>> selector) => source.Select(selector).Switch();
+        public static IObservable<TResult> SelectSwitch<T, TResult>(this IObservable<T> This, Func<T, int, IObservable<TResult>> selector) => This.Select(selector).Switch();
 
         /// <summary>
         /// Catches any error that might occur and return an observable with a single given value.
         /// </summary>
         /// <returns>An observable with a single given value.</returns>
-        /// <param name="source">The source observable to catch errors on.</param>
+        /// <param name="This">The source observable to catch errors on.</param>
         /// <param name="returnValue">The value to be sent in the returning observable.</param>
         /// <typeparam name="T">The type of the signal inside the observable.</typeparam>
-        public static IObservable<T> CatchAndReturn<T>(this IObservable<T> source, T returnValue) => source.Catch(Observable.Return(returnValue));
+        public static IObservable<T> CatchAndReturn<T>(this IObservable<T> This, T returnValue) => This.Catch(Observable.Return(returnValue));
 
         /// <summary>
         /// Syntactic sugar for filtering an <c>IObservable&lt;bool&gt;</c> to only contain <c>true</c> or <c>false</c> signals.
         /// </summary>
         /// <returns>An <c>IObservable&lt;bool&gt;</c> that only contains the specified boolean value.</returns>
-        /// <param name="source">The observable to filter.</param>
+        /// <param name="This">The observable to filter.</param>
         /// <param name="condition">The boolean value to check against.</param>
-        public static IObservable<bool> Where(this IObservable<bool> source, bool condition) => source.Where(value => value == condition);
+        public static IObservable<bool> Where(this IObservable<bool> This, bool condition) => This.Where(value => value == condition);
 
         /// <summary>
         /// Syntactic sugar for filtering an observable to only contain the specified constant.
         /// </summary>
-        /// <param name="source">The observable to filter.</param>
+        /// <param name="This">The observable to filter.</param>
         /// <param name="condition">A constant to check the values of the observable against.</param>
         /// <typeparam name="T">The type of the signals of the observable</typeparam>
         /// <returns>An observable that only contains values equal to the specified constant.</returns>
-        public static IObservable<T> Where<T>(this IObservable<T> source, T condition) => source.Where(value => value.Equals(condition));
+        public static IObservable<T> Where<T>(this IObservable<T> This, T condition) => This.Where(value => value.Equals(condition));
 
         /// <summary>
         /// Syntactic sugar for filtering an <c>IObservable&lt;T&gt;</c> to only contain non-null singals.
         /// </summary>
         /// <returns>An <c>IObservable&lt;T&gt;</c> that only contains non-null signals.</returns>
-        /// <param name="source">The observable to filter.</param>
+        /// <param name="This">The observable to filter.</param>
         /// <typeparam name="T">The the of the signal inside the observable.</typeparam>
-        public static IObservable<T> WhereNotNull<T>(this IObservable<T> source) => source.Where(value => value != null);
+        public static IObservable<T> WhereNotNull<T>(this IObservable<T> This) => This.Where(value => value != null);
 
         /// <summary>
         /// Writes every signal of the source observable to the debug console (without modifying the source observable)
         /// </summary>
         /// <returns>The source observable.</returns>
-        /// <param name="source">The source observable.</param>
+        /// <param name="This">The source observable.</param>
         /// <param name="text">A text to prefix the console message with.</param>
         /// <typeparam name="T">The type of the source observable.</typeparam>
-		public static IObservable<T> Debug<T>(this IObservable<T> source, string text = "") => source.Do(x => System.Diagnostics.Debug.WriteLine($"{text}{(text.HasValue() ? " " : "")}OnNext: {x}"), ex => System.Diagnostics.Debug.WriteLine($"{text}{(text.HasValue() ? " " : "")}OnError: {ex}"), () => System.Diagnostics.Debug.WriteLine($"{text}{(text.HasValue() ? " " : "")}OnCompleted"));
+		public static IObservable<T> Debug<T>(this IObservable<T> This, string text = "") => This.Do(x => System.Diagnostics.Debug.WriteLine($"{text}{(text.HasValue() ? " " : "")}OnNext: {x}"), ex => System.Diagnostics.Debug.WriteLine($"{text}{(text.HasValue() ? " " : "")}OnError: {ex}"), () => System.Diagnostics.Debug.WriteLine($"{text}{(text.HasValue() ? " " : "")}OnCompleted"));
 
         /// <summary>
         /// Writes every signal of the source observable to the debug console by applying the selector (without modidying the source observable)
         /// </summary>
         /// <returns>The source observable.</returns>
-        /// <param name="source">The source observable.</param>
+        /// <param name="This">The source observable.</param>
         /// <param name="selector">A function to select the desired property of the signal.</param>
         /// <param name="text">A text to prefix the console message with.</param>
         /// <typeparam name="T">The type of the source observable.</typeparam>
         /// <typeparam name="TResult">The type of the result of the selector, which is written to the console.</typeparam>
-        public static IObservable<T> Debug<T, TResult>(this IObservable<T> source, Func<T, TResult> selector, string text = "") => source.Do(x => System.Diagnostics.Debug.WriteLine($"{text}{(text.HasValue() ? " " : "")}OnNext: {selector(x)}"), ex => System.Diagnostics.Debug.WriteLine($"{text}{(text.HasValue() ? " " : "")}OnError: {ex}"), () => System.Diagnostics.Debug.WriteLine($"{text}{(text.HasValue() ? " " : "")}OnCompleted"));
+        public static IObservable<T> Debug<T, TResult>(this IObservable<T> This, Func<T, TResult> selector, string text = "") => This.Do(x => System.Diagnostics.Debug.WriteLine($"{text}{(text.HasValue() ? " " : "")}OnNext: {selector(x)}"), ex => System.Diagnostics.Debug.WriteLine($"{text}{(text.HasValue() ? " " : "")}OnError: {ex}"), () => System.Diagnostics.Debug.WriteLine($"{text}{(text.HasValue() ? " " : "")}OnCompleted"));
     }
 }
